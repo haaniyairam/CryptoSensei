c3f24c25e1a27302195c0a1e23119fe26a1663c6
haaniyairam <haaniyairam@gmail.com>
2023-04-11 20:10:14 +0530

    cool

---
+++ pages/api/generate.js
@@ -0,0 +1,52 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `
+Write a report in the style of warren buffet please make sure the report is understandable by a common man
+`;
+
+const generateAction = async (req, res) => {
+	console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+	const baseCompletion = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+		temperature: 0.8,
+		max_tokens: 250,
+	});
+
+	const basePromptOutput = baseCompletion.data.choices.pop();
+
+	// I build Prompt #2.
+	const secondPrompt = `explain every concept with real life relatable examples. Don't just list the points. Go deep into each one. Explain why.
+
+ Title: ${req.body.userInput}
+
+ Table of Contents: ${basePromptOutput.text}
+
+ Blog Post:
+ `;
+
+	// I call the OpenAI API a second time with Prompt #2
+	const secondPromptCompletion = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: `${secondPrompt}`,
+		// I set a higher temperature for this one. Up to you!
+		temperature: 0.85,
+		// I also increase max_tokens.
+		max_tokens: 1250,
+	});
+
+	// Get the output
+	const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+	// Send over the Prompt #2's output to our UI instead of Prompt #1's.
+	res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;

aec7a64d2fb7d92082a514969d86384e9ee0df14
haaniyairam <haaniyairam@gmail.com>
2023-04-26 21:24:15 +0530

    cool

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ const callGenerateEndpoint = async () => {
   <div className="output">
     <div className="output-header-container">
       <div className="output-header">
-        <h3>Output</h3>
+        <h3>So What is it?</h3>
       </div>
     </div>
     <div className="output-content">
